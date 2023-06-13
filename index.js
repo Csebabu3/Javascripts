a6fd771865f890148d28bee1b5b393a61934d673
Csebabu3 <csebabu3@gmail.com>
2023-06-13 17:07:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const name="learning";
+const dob="19-10-2001";
+let msg="i have " + name + " & i born on " + dob +" building js";
+alert(msg);      
+console.log(msg);
+let number1=5;
+let number2=6;
+let result=number1+number2;
+alert(result);
+console.log(result); 
+let firstname="babu"
+let age=21;
+let isyoung=true //boolean
